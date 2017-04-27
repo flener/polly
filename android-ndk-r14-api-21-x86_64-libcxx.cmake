b7c223f2e164fc6fbcbc5242d507fb0e08b6a633
Fernando Lener <nando0205@gmail.com>
2017-04-27 15:07:16 -0300

    Updating compiler to clang

--- android-ndk-r14-api-21-x86_64-libcxx.cmake
+++ android-ndk-r14-api-21-x86_64-libcxx.cmake
@@ -2,10 +2,10 @@
 # Copyright (c) 2015, David Hirvonen
 # All rights reserved.
 
-if(DEFINED POLLY_ANDROID_NDK_R14_API_21_ARMEABI_NEON_LIBCXX_CMAKE_)
+if(DEFINED POLLY_ANDROID_NDK_R14_API_21_X86_64_NEON_LIBCXX_CMAKE_)
   return()
 else()
-    set(POLLY_ANDROID_NDK_R14_API_21_ARMEABI_NEON_LIBCXX_CMAKE_ 1)
+    set(POLLY_ANDROID_NDK_R14_API_21_X86_64_NEON_LIBCXX_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
@@ -14,7 +14,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 set(ANDROID_NDK_VERSION "r14")
 set(CMAKE_SYSTEM_VERSION "21")
-set(CMAKE_ANDROID_ARCH_ABI "armeabi")
+set(CMAKE_ANDROID_ARCH_ABI "x86_64")
 set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION "clang")
 set(CMAKE_ANDROID_STL_TYPE "c++_static") # LLVM libc++ static
 
